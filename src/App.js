6eb880722938fc665ecc55b6c2b5a546a8ecdcfd
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-06-30 19:33:17 -0400

    added product/home site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import Home from "./Home";
 
 // BEM
 function App() {
   return (
     <div className="app">
     <Header />
-    {/* Home */}
+    <Home />
     </div>
   );
 }
