ab777889b8d4ffc2160c1bc51295c36a73c6080f
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-06-30 20:12:26 -0400

    product props

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className='product'>
         <div className='product__info'>
-            <p> The lean startup </p>
+            <p>{title}</p>
             <p className='product__price'>
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className='product__rating'>
-                <p>⭐</p>
+            {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+                ))}
             </div>
         </div>
 
-        <img src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg' alt='book-artwork' />
+        <img src={image} alt='' />
 
         <button>Add to Basket</button>
     </div>
