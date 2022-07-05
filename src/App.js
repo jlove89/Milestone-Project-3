bd3f26283bc94d7f05057f4aba42b42298a71d95
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-07-05 19:06:12 -0400

    login page done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Home from "./Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Checkout from "./Checkout";
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Checkout from './Checkout';
+import Login from './Login'
 
 // BEM
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header/>
         <Switch>
+        <Route path="/login">
+          <Login/>
+          </Route>
         <Route path="/checkout">
+          <Header/>
           <Checkout/>
           </Route>
         <Route path="/">
-            <Home/>
+          <Header/>
+          <Home/>
           </Route>
         </Switch>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import { useStateValue } from './StateProvider'
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue()
+  const [{ basket }, /*dispatch*/] = useStateValue()
 
   return (
     <div className='header'>
@@ -20,10 +20,12 @@ function Header() {
         </div>
 
         <div className='header__nav'>
+            <Link to='/login'>
             <div className='header__option'>
                 <span className='header__optionLineOne'>Hello Guest</span>
                 <span className='header__optionLineTwo'>Sign In</span>
             </div>
+            </Link>
 
             <div className='header__option'>
             <span className='header__optionLineOne'>Returns</span>
